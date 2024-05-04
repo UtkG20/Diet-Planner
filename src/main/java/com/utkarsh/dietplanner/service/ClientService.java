233834ff7e1e2802a5c8e1f591478ecb9e1cff05
unknown <goyalgoyal9784@gmail.com>
2024-05-04 19:21:32 +0530

    defrbgh

--- src/main/java/com/utkarsh/dietplanner/service/ClientService.java
+++ src/main/java/com/utkarsh/dietplanner/service/ClientService.java
@@ -3,15 +3,25 @@ package com.utkarsh.dietplanner.service;
 import com.utkarsh.dietplanner.Models.Client;
 import com.utkarsh.dietplanner.dao.ClientDao;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
-public class ClientService {
+public class ClientService implements UserDetailsService {
 
     @Autowired
     ClientDao clientDao;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        Client client;
+        client = clientDao.findByUsername(username.toString());
+        return client;
+    }
 
     public List<Client> getAllClients() {
         return clientDao.findAll();
@@ -31,4 +41,20 @@ public class ClientService {
             System.out.println(e);
         }
     }
+
+    public String updateUser(Integer clientId, Client user) {
+        Client client = clientDao.findById(clientId).get();
+
+        client.setAge(user.getAge());
+        client.setUsername(user.getUsername());
+        client.setPassword((user.getPassword()));
+        return client.getUsername() + " details updated successfully..";
+    }
+
+    public Client getUserByUsername(String name) {
+        Client client = clientDao.findByUsername(name);
+        return client;
+    }
+
+
 }
